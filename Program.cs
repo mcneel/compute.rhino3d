1ecc65b47d22b1cb0baa8daf8872c99da39db715
Steve Baer <steve@mcneel.com>
2018-01-26 11:13:24 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using Nancy.Extensions;
+using Topshelf;
+
+namespace RhinoCommon.Rest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int port = 80;
+      string secret = null;
+      bool https = false;
+      Topshelf.HostFactory.Run(x =>
+      {
+        x.AddCommandLineDefinition("port", p => port = int.Parse(p));
+        x.AddCommandLineDefinition("secret", s => secret = s);
+        x.AddCommandLineDefinition("https", b => https = bool.Parse(b));
+        x.ApplyCommandLine();
+        x.SetStartTimeout(new TimeSpan(0, 1, 0));
+        x.Service<NancySelfHost>(s =>
+        {
+          s.ConstructUsing(name => new NancySelfHost());
+          s.WhenStarted(tc => tc.Start(https, port, secret));
+          s.WhenStopped(tc => tc.Stop());
+        });
+        x.RunAsPrompt();
+        //x.RunAsLocalService();
+        x.SetDisplayName("RhinoCommon Geometry Server");
+        x.SetServiceName("RhinoCommon Geometry Server");
+      });
+      RhinoLib.ExitInProcess();
+    }
+  }
+
+  public class NancySelfHost
+  {
+    private NancyHost _nancyHost;
+
+    public void Start(bool https, int port, string secret)
+    {
+      RhinoModule.Secret = secret;
+      Console.WriteLine($"Launching RhinoCore library as {Environment.UserName}");
+      RhinoLib.LaunchInProcess(0, 0);
+      var config = new HostConfiguration();
+      string address = https ? $"https://localhost:{port}" :
+                               $"http://localhost:{port}";
+      _nancyHost = new NancyHost(config, new Uri(address));
+      _nancyHost.Start();
+      Console.WriteLine("Running on " + address);
+    }
+
+    public void Stop()
+    {
+      _nancyHost.Stop();
+    }
+  }
+
+  public class RhinoModule : Nancy.NancyModule
+  {
+    public static string Secret { get; set; }
+
+    Nancy.HttpStatusCode CheckSecret()
+    {
+      if (string.IsNullOrWhiteSpace(Secret))
+        return Nancy.HttpStatusCode.OK;
+      var request_secret = new System.Collections.Generic.List<string>(this.Request.Headers["secret"]);
+      if (request_secret[0].Equals(Secret, StringComparison.Ordinal))
+        return Nancy.HttpStatusCode.OK;
+      return Nancy.HttpStatusCode.Unauthorized;
+    }
+
+    public RhinoModule()
+    {
+      var endpoints = EndPointDictionary.GetDictionary();
+      foreach(var kv in endpoints)
+      {
+        Get[kv.Key] = _ =>
+        {
+          var authCheck = CheckSecret();
+          if (authCheck != Nancy.HttpStatusCode.OK)
+            return authCheck;
+          return kv.Value.HandleGet();
+        };
+        Post[kv.Key] = _ =>
+        {
+          var authCheck = CheckSecret();
+          if (authCheck != Nancy.HttpStatusCode.OK)
+            return authCheck;
+          var jsonString = Request.Body.AsString();
+          return kv.Value.HandlePost(jsonString);
+        };
+      }
+    }
+  }
+}
---
+++ deployment/downloadrhino.ps1
@@ -0,0 +1,3 @@
+﻿$url = "http://files.mcneel.com/dujour/exe/20171215/rhino_en-us_6.1.17349.22391.exe"
+$output = "rhino6install.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
