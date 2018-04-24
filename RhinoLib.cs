f30a6d2d3a1c762cafb8eb15b053a673f87c9d26
Steve Baer <steve@mcneel.com>
2018-04-24 02:14:07 -0700

    Update RhinoLib to allow use of the Rhino WIP with local debug builds

--- RhinoLib.cs
+++ RhinoLib.cs
@@ -15,8 +15,17 @@ class RhinoLib
         if (!_pathsSet)
         {
             string envPath = Environment.GetEnvironmentVariable("path");
+
+            // Add Rhino WIP to the path. Let debug use the internal debug build of Rhino
+            // for internal McNeel development. For people without access to internal builds,
+            // use the Rhino WIP directory for both debug and release builds
 #if DEBUG
             string rhinoSystemDir = @"C:\dev\github\mcneel\rhino\src4\bin\Debug";
+            if(!System.IO.File.Exists(rhinoSystemDir))
+            {
+                string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                rhinoSystemDir = System.IO.Path.Combine(programFiles, "Rhino WIP", "System");
+            }
 #else
             string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
             string rhinoSystemDir = System.IO.Path.Combine(programFiles, "Rhino WIP", "System");
