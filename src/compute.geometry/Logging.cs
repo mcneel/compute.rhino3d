2a8c0ce81fa250d64e66185e9997502699c60974
Will Pearson <will@mcneel.com>
2018-09-30 14:14:11 +0100

    Add cloudwatch log sink
    Enabled via COMPUTE_LOG_CLOUDWATCH=true
    
    See COMPUTE-22

--- src/compute.geometry/Logging.cs
+++ src/compute.geometry/Logging.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.CloudWatchLogs;
 using Serilog;
+using Serilog.Events;
 using Serilog.Formatting.Json;
+using Serilog.Sinks.AwsCloudWatch;
 
 namespace compute.geometry
 {
@@ -17,19 +20,50 @@ namespace compute.geometry
             if (_enabled)
                 return;
 
-            var path = Path.Combine(Path.GetTempPath(), "Compute", "Logs", "log-geometry-.txt"); // log-20180925.txt, etc.
+            var path = Path.Combine(Path.GetTempPath(), "Compute", "Logs", "log-frontend-.txt"); // log-20180925.txt, etc.
             var limit = Env.GetEnvironmentInt("COMPUTE_LOG_RETAIN_DAYS", 10);
 
-            Log.Logger = new LoggerConfiguration()
+            var logger = new LoggerConfiguration()
 #if DEBUG
                 .MinimumLevel.Debug()
 #endif
                 .Enrich.FromLogContext()
                 .Enrich.WithProperty("Source", "geometry")
                 .WriteTo.Console(outputTemplate: "{Timestamp:o} {Level:w3}: {Source} {Message:lj} {Properties:j}{NewLine}{Exception}")
-                .WriteTo.File(new JsonFormatter(), path, rollingInterval: RollingInterval.Day, retainedFileCountLimit: limit)
-                // TODO: cloudwatch, stackdriver
-                .CreateLogger();
+                .WriteTo.File(new JsonFormatter(), path, rollingInterval: RollingInterval.Day, retainedFileCountLimit: limit);
+
+            var cloudwatch_enabled = false;
+            var aws_log_group = Env.GetEnvironmentString("COMPUTE_LOG_CLOUDWATCH_GROUP", "/compute/debug");
+
+            if (Env.GetEnvironmentBool("COMPUTE_LOG_CLOUDWATCH", true))
+            {
+                var options = new CloudWatchSinkOptions
+                {
+                    LogGroupName = aws_log_group,
+                    MinimumLogEventLevel = LogEventLevel.Debug,
+                    TextFormatter = new JsonFormatter(),
+                    LogGroupRetentionPolicy = LogGroupRetentionPolicy.SixMonths,
+#if !DEBUG
+                    Period = TimeSpan.FromSeconds(60)
+#endif
+                };
+
+                var aws_region_string = Env.GetEnvironmentString("AWS_REGION_ENDPOINT", "us-east-1");
+                var aws_region_endpoint = Amazon.RegionEndpoint.GetBySystemName(aws_region_string);
+                var aws_client = new AmazonCloudWatchLogsClient(aws_region_endpoint);
+
+                logger.WriteTo.AmazonCloudWatch(options, aws_client);
+
+                cloudwatch_enabled = true;
+            }
+
+            Log.Logger = logger.CreateLogger();
+
+            Log.Debug("Logging to {LogPath}", Path.GetDirectoryName(path));
+
+            if (cloudwatch_enabled)
+                Log.ForContext("LogGroup", aws_log_group)
+                    .Debug("Amazon CloudWatch logging enabled");
 
             _enabled = true;
         }
