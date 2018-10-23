f299a601a1ef9ed6aaa3386c284a9c9af29b1c2b
Steve Baer <steve@mcneel.com>
2018-10-23 16:22:28 -0700

    roll python client version number

--- src/compute.client/python_client/setup.py
+++ src/compute.client/python_client/setup.py
@@ -31,7 +31,7 @@ Project Hompage at: https://github.com/mcneel/compute.rhino3d
 
 setuptools.setup(
     name="compute_rhino3d",
-    version="0.0.4",
+    version="0.0.5",
     packages=['compute_rhino3d'],
     author="Robert McNeel & Associates",
     author_email="steve@mcneel.com",
