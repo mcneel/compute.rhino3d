8e79063c9a1a96bc570dd2b5af4d35115a7418c5
Enmerkar <p.i.g.a.c.h@gmail.com>
2018-11-14 23:10:25 -0500

    Schema

---
+++ src/Resthopper.IO/GhPath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resthopper.IO
+{
+    public class GhPath
+    {
+        int[] _path;
+
+        public GhPath(int[] path)
+        {
+            _path = path;
+        }
+
+        public GhPath(GhPath pathObj, int i)
+        {
+            int[] path = pathObj._path;
+            _path = new int[path.Length + 1];
+
+            for (int j = 0; j < path.Length; j++)
+            {
+                _path[j] = path[j];
+            }
+            _path[path.Length] = i;
+        }
+
+        public bool LastIndexSame(int i)
+        {
+            return _path.Last() == i;
+        }
+    }
+}
---
+++ src/Resthopper.IO/Schema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Resthopper.IO
+{
+    class Schema
+    {
+        
+        [JsonProperty(PropertyName = "algo")]
+        public string Algo { get; set; }
+
+        [JsonProperty(PropertyName = "values")]
+        public Dictionary<string, Dictionary<GhPath, object>> Values { get; set; }
+        
+    }
+}
