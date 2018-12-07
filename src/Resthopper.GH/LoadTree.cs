2aa7c33e252c569762c307251043cdd0e11ed56a
Enmerkar <p.i.g.a.c.h@gmail.com>
2018-12-06 21:44:52 -0500

    GH Tools for debugging Compute server

---
+++ src/Resthopper.GH/LoadTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.IO;
+using Newtonsoft.Json;
+using Grasshopper;
+using Grasshopper.Kernel.Types;
+using Resthopper.IO;
+
+namespace Resthopper.GH
+{
+    public class LoadTree : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the LoadTree class.
+        /// </summary>
+        public LoadTree()
+          : base("Load Tree", "Load Tree",
+              "Description",
+              "Resthopper", "Debug")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("path", "path", "path to saved tree", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("tree", "tree", "loaded tree", GH_ParamAccess.tree);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string path = string.Empty;
+            DA.GetData(0, ref path);
+            Resthopper.IO.DataTree<ResthopperObject> tree;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string serialized = reader.ReadToEnd();
+                tree = JsonConvert.DeserializeObject<Resthopper.IO.DataTree<ResthopperObject>> (serialized);
+            }
+
+            Grasshopper.Kernel.Data.GH_Structure<IGH_Goo> ghTree = new Grasshopper.Kernel.Data.GH_Structure<IGH_Goo>();
+            foreach (KeyValuePair<string, List<ResthopperObject>> pair in tree)
+            {
+                List<IGH_Goo> pLines = new List<IGH_Goo>();
+                Grasshopper.Kernel.Data.GH_Path ghPath = new Grasshopper.Kernel.Data.GH_Path(GhPath.FromString(pair.Key));
+                foreach (ResthopperObject ro in pair.Value)
+                {
+
+                    try { ghTree.Append(new GH_Curve(JsonConvert.DeserializeObject<Curve>(ro.Data)), ghPath); }
+                    catch
+                    {
+                        try { ghTree.Append(new GH_Point(JsonConvert.DeserializeObject<Point3d>(ro.Data)), ghPath); }
+                        catch
+                        {
+                            ghTree.Append(new GH_Boolean(JsonConvert.DeserializeObject<bool>(ro.Data)), ghPath);
+                        }
+                    }
+                }
+                
+            }
+                    DA.SetDataTree(0, ghTree);
+                 
+            
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("f9c34ede-a64f-43ef-806d-314443637b54"); }
+        }
+    }
+}
---
+++ src/Resthopper.GH/SaveTree.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Grasshopper;
+using Grasshopper.Kernel.Types;
+using Newtonsoft.Json;
+using System.IO;
+using Resthopper.IO;
+
+namespace Resthopper.GH
+{
+    public class SaveTree : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the SaveTree class.
+        /// </summary>
+        public SaveTree()
+          : base("Save Tree", "Save Tree",
+              "Description",
+              "Resthopper", "Debug")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("tree", "tree", "tree to serialize", GH_ParamAccess.tree);
+            pManager.AddTextParameter("path", "path", "path to save the file", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("tree", "tree", "tree to path through", GH_ParamAccess.tree);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Grasshopper.Kernel.Data.GH_Structure<IGH_Goo> tree = new Grasshopper.Kernel.Data.GH_Structure<IGH_Goo>();
+            string path = string.Empty;
+
+            DA.GetDataTree(0, out tree);
+            DA.GetData(1, ref path);
+            Resthopper.IO.DataTree<ResthopperObject> OutputTree = new Resthopper.IO.DataTree<ResthopperObject>();
+
+            var volatileData = this.Params.Input[0].Sources[0].VolatileData;
+            for (int p = 0; p < volatileData.PathCount; p++)
+            {
+                List<ResthopperObject> ResthopperObjectList = new List<ResthopperObject>();
+                foreach (var goo in volatileData.get_Branch(p))
+                {
+                    if (goo == null) continue;
+                    else if (goo.GetType() == typeof(GH_Boolean))
+                    {
+                        GH_Boolean ghValue = goo as GH_Boolean;
+                        bool rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<bool>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Point))
+                    {
+                        GH_Point ghValue = goo as GH_Point;
+                        Point3d rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Point3d>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Vector))
+                    {
+                        GH_Vector ghValue = goo as GH_Vector;
+                        Vector3d rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Vector3d>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Integer))
+                    {
+                        GH_Integer ghValue = goo as GH_Integer;
+                        int rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<int>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Number))
+                    {
+                        GH_Number ghValue = goo as GH_Number;
+                        double rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<double>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_String))
+                    {
+                        GH_String ghValue = goo as GH_String;
+                        string rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<string>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Line))
+                    {
+                        GH_Line ghValue = goo as GH_Line;
+                        Line rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Line>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Curve))
+                    {
+                        GH_Curve ghValue = goo as GH_Curve;
+                        Curve rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Curve>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Circle))
+                    {
+                        GH_Circle ghValue = goo as GH_Circle;
+                        Circle rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Circle>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Plane))
+                    {
+                        GH_Plane ghValue = goo as GH_Plane;
+                        Plane rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Plane>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Rectangle))
+                    {
+                        GH_Rectangle ghValue = goo as GH_Rectangle;
+                        Rectangle3d rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Rectangle3d>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Box))
+                    {
+                        GH_Box ghValue = goo as GH_Box;
+                        Box rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Box>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Surface))
+                    {
+                        GH_Surface ghValue = goo as GH_Surface;
+                        Brep rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Brep>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Brep))
+                    {
+                        GH_Brep ghValue = goo as GH_Brep;
+                        Brep rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Brep>(rhValue));
+                    }
+                    else if (goo.GetType() == typeof(GH_Mesh))
+                    {
+                        GH_Mesh ghValue = goo as GH_Mesh;
+                        Mesh rhValue = ghValue.Value;
+                        ResthopperObjectList.Add(GetResthopperObject<Mesh>(rhValue));
+                    }
+                }
+
+                GhPath ghpath = new GhPath(new int[] { p });
+                OutputTree.Add(ghpath.ToString(), ResthopperObjectList);
+            }
+
+
+
+
+
+
+            string serialized = JsonConvert.SerializeObject(OutputTree);
+            using(StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(serialized);
+            }
+            DA.SetDataTree(0, tree);
+        }
+
+        public static ResthopperObject GetResthopperObject<T>(object goo)
+        {
+            var v = (T)goo;
+
+            ResthopperObject rhObj = new ResthopperObject();
+            rhObj.Type = goo.GetType().FullName;
+            rhObj.Data = JsonConvert.SerializeObject(v);
+            return rhObj;
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("8fda0216-6111-49b6-93f5-e7c82b2ce1c1"); }
+        }
+    }
+}
