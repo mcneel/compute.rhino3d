92ef9c820eeced645d6248e76f31ee98ecff2f00
enmerk4r <p.i.g.a.c.h@gmail.com>
2019-01-10 18:22:42 -0500

    Removing Resthopper Pipeline

---
+++ src/compute.geometry/IO/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Resthopper.IO
+{
+
+    public static class Utils
+    {
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
