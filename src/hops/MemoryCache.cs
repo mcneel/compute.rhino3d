b2c94af63ed486456fe32645db701c87ce2fb6f6
Steve Baer <steve@mcneel.com>
2021-03-13 08:28:15 -0800

    Add options to turn on/off memory cache per component

---
+++ src/hops/MemoryCache.cs
@@ -0,0 +1,22 @@
+﻿using Resthopper.IO;
+
+namespace Hops
+{
+    /// <summary>
+    /// Use a separate class for all memory caching. This allows us to control how
+    /// caching is performed in the future and to clear a cache if we want.
+    /// </summary>
+    static class MemoryCache
+    {
+        public static Schema Get(string key)
+        {
+            var cachedResults = System.Runtime.Caching.MemoryCache.Default.Get(key) as Schema;
+            return cachedResults;
+        }
+
+        public static void Set(string key, Schema schema)
+        {
+            System.Runtime.Caching.MemoryCache.Default.Set(key, schema, new System.Runtime.Caching.CacheItemPolicy());
+        }
+    }
+}
