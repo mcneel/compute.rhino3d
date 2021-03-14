28bb78ae3dbacd4b910403c961da8a3f43246007
Steve Baer <steve@mcneel.com>
2021-03-13 21:36:19 -0800

    add more param types for python

--- src/ghhops-server-py/ghhops_server/params.py
+++ src/ghhops-server-py/ghhops_server/params.py
@@ -8,10 +8,36 @@ from ghhops_server.logger import hlogger
 
 __all__ = (
     "HopsParamAccess",
-    "HopsNumber",
+    # "HopsArc",
+    "HopsBoolean",
+    # "HopsBox",
+    "HopsBrep",
+    # "HopsCircle",
+    # "HopsColour",
+    # "HopsComplex"
+    # "HopsCulture",
     "HopsCurve",
+    # "HopsField",
+    # "HopsFilePath",
+    # "HopsGeometry",
+    "HopsInteger",
+    # "HopsInterval",
+    # "HopsInterval2D"
+    "HopsLine",
+    # "HopsMatrix",
+    "HopsMesh",
+    # "HopsMeshFace",
+    "HopsNumber",
+    # "HopsPlane",
     "HopsPoint",
+    # "HopsRectangle",
+    "HopsString",
+    # "HopsStructurePath",
+    "HopsSubD",
     "HopsSurface",
+    # "HopsTime",
+    # "HopsTransform",
+    "HopsVector"
 )
 
 
@@ -157,6 +183,60 @@ class _GHParam:
         return output
 
 
+class HopsBoolean(_GHParam):
+    """Wrapper for GH_Boolean"""
+    param_type = "Boolean"
+    result_type = "System.Boolean"
+
+    coercers = {
+        "System.Boolean": lambda b: bool(b)
+    }
+
+
+class HopsBrep(_GHParam):
+    """Wrapper for GH Brep"""
+    param_type = "Brep"
+    result_type = "Rhino.Geometry.Brep"
+
+
+class HopsCurve(_GHParam):
+    """Wrapper for GH Curve"""
+    param_type = "Curve"
+    result_type = "Rhino.Geometry.Curve"
+
+
+class HopsInteger(_GHParam):
+    """Wrapper for GH_Integer"""
+    param_type = "Integer"
+    result_type = "System.Int32"
+
+    coercers = {
+        "System.Int32": lambda i: int(i)
+    }
+
+
+class HopsLine(_GHParam):
+    """Wrapper for GH_Line"""
+    param_type = "Line"
+    result_type = "Rhino.Geometry.Line"
+
+    coercers = {
+        "Rhino.Geometry.Line": lambda l: RHINO_GEOM.Line(
+            HopsLine._make_point(l["From"]), HopsLine._make_point(l["To"])
+        )
+    }
+
+    @staticmethod
+    def _make_point(a):
+        return RHINO_GEOM.Point3d(a["X"], a["Y"], a["Z"])
+
+
+class HopsMesh(_GHParam):
+    """Wrapper for GH Mesh"""
+    param_type = "Mesh"
+    result_type = "Rhino.Geometry.Mesh"
+
+
 class HopsNumber(_GHParam):
     """Wrapper for GH Number"""
 
@@ -168,16 +248,8 @@ class HopsNumber(_GHParam):
     }
 
 
-class HopsCurve(_GHParam):
-    """Wrapper for GH Curve"""
-
-    param_type = "Curve"
-    result_type = "Rhino.Geometry.Curve"
-
-
 class HopsPoint(_GHParam):
     """Wrapper for GH Point"""
-
     param_type = "Point"
     result_type = "Rhino.Geometry.Point3d"
 
@@ -192,8 +264,40 @@ class HopsPoint(_GHParam):
     }
 
 
+class HopsString(_GHParam):
+    """Wrapper for GH_String"""
+    param_type = "Text"
+    result_type = "System.String"
+
+    coercers = {
+        "System.String": lambda s: s
+    }
+
+
+class HopsSubD(_GHParam):
+    """Wrapper for GH SubD"""
+    param_type = "SubD"
+    result_type = "Rhino.Geometry.SubD"
+
+
 class HopsSurface(_GHParam):
     """Wrapper for GH Surface"""
 
     param_type = "Surface"
     result_type = "Rhino.Geometry.Brep"
+
+
+class HopsVector(_GHParam):
+    """Wrapper for GH Vector"""
+    param_type = "Vector"
+    result_type = "Rhino.Geometry.Vcetor3d"
+
+    coercers = {
+        "Rhino.Geometry.Point2d": lambda d: RHINO_GEOM.Point2d(d["X"], d["Y"]),
+        "Rhino.Geometry.Point3d": lambda d: RHINO_GEOM.Point3d(
+            d["X"], d["Y"], d["Z"]
+        ),
+        "Rhino.Geometry.Vector3d": lambda d: RHINO_GEOM.Vector3d(
+            d["X"], d["Y"], d["Z"]
+        ),
+    }
