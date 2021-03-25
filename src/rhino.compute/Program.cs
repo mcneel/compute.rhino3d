ac63641cae27b00f43ed92df50fb9ba6e25cb24d
Steve Baer <steve@mcneel.com>
2021-03-25 07:06:41 -0700

    Include rhino.compute with Hops

--- src/rhino.compute/Program.cs
+++ src/rhino.compute/Program.cs
@@ -1,5 +1,6 @@
 namespace rhino.compute
 {
+    using System;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.DependencyInjection;
@@ -12,9 +13,9 @@ namespace rhino.compute
         {
             [Option("childof",
              Required = false,
-             HelpText = @"Process name of parent process. Compute watches for the existence 
-of any process by this name and if there are none; it will shut down")]
-            public string ChildOf { get; set; }
+             HelpText = @"Process Handle of parent process. Compute watches for the existence 
+of this handle and will shut down when this process has exited")]
+            public int ChildOf { get; set; }
 
             [Option("childcount",
              Required = false,
@@ -32,7 +33,7 @@ of any process by this name and if there are none; it will shut down")]
             public int Port { get; set; } = -1;
         }
 
-        static string _parentProcessName;
+        static System.Diagnostics.Process _parentProcess;
         static System.Timers.Timer _selfDestructTimer;
 
         public static void Main(string[] args)
@@ -42,10 +43,9 @@ of any process by this name and if there are none; it will shut down")]
             {
                 ComputeChildren.SpawnCount = o.ChildCount;
                 ComputeChildren.ChildIdleSpan = new System.TimeSpan(0, 0, o.IdleSpanSeconds);
-                _parentProcessName = o.ChildOf;
+                _parentProcess = System.Diagnostics.Process.GetProcessById(o.ChildOf);
                 port = o.Port;
             });
-
             var host = Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
@@ -56,18 +56,20 @@ of any process by this name and if there are none; it will shut down")]
                         ComputeChildren.ParentPort = port;
                     }
                 }).Build();
-            
+
             var logger = host.Services.GetRequiredService<ILogger<ReverseProxyModule>>();
             ReverseProxyModule.InitializeConcurrentRequestLogging(logger);
 
-            if (!string.IsNullOrWhiteSpace(_parentProcessName))
+            if (_parentProcess != null)
             {
                 _selfDestructTimer = new System.Timers.Timer(1000);
                 _selfDestructTimer.Elapsed += (s, e) =>
                 {
-                    var procs = System.Diagnostics.Process.GetProcessesByName(_parentProcessName);
-                    if (procs==null || procs.Length<1)
+                    if (_parentProcess.HasExited)
                     {
+                        _selfDestructTimer.Stop();
+                        _parentProcess = null;
+                        Console.WriteLine("self-destruct");
                         host.StopAsync();
                     }
                 };
