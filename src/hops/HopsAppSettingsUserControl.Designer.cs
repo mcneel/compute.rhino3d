fae5ba2678e6e08ebf828b338b773fefacdb3e23
Andrew Payne <andy.payne@mcneel.com>
2022-01-21 12:08:20 -0500

    adding API Key dialog to hops preferences. Including api key in post request header

--- src/hops/HopsAppSettingsUserControl.Designer.cs
+++ src/hops/HopsAppSettingsUserControl.Designer.cs
@@ -29,116 +29,152 @@ namespace Hops
         /// </summary>
         private void InitializeComponent()
         {
-      this.components = new System.ComponentModel.Container();
-      this._serversTextBox = new System.Windows.Forms.TextBox();
-      this._hideWorkerWindows = new System.Windows.Forms.CheckBox();
-      this._launchWorkerAtStart = new System.Windows.Forms.CheckBox();
-      this._btnClearMemCache = new System.Windows.Forms.Button();
-      this._lblCacheCount = new System.Windows.Forms.Label();
-      this._childComputeCount = new System.Windows.Forms.NumericUpDown();
-      this._updateChildCountButton = new System.Windows.Forms.Button();
-      this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-      this._maxConcurrentRequestsTextbox = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this._childComputeCount)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // _serversTextBox
-      // 
-      this._serversTextBox.Location = new System.Drawing.Point(3, 1);
-      this._serversTextBox.Multiline = true;
-      this._serversTextBox.Name = "_serversTextBox";
-      this._serversTextBox.Size = new System.Drawing.Size(586, 91);
-      this._serversTextBox.TabIndex = 1;
-      // 
-      // _hideWorkerWindows
-      // 
-      this._hideWorkerWindows.AutoSize = true;
-      this._hideWorkerWindows.Location = new System.Drawing.Point(3, 213);
-      this._hideWorkerWindows.Name = "_hideWorkerWindows";
-      this._hideWorkerWindows.Size = new System.Drawing.Size(409, 29);
-      this._hideWorkerWindows.TabIndex = 2;
-      this._hideWorkerWindows.Text = "Hide Rhino.Compute Console Window";
-      this._hideWorkerWindows.UseVisualStyleBackColor = true;
-      // 
-      // _launchWorkerAtStart
-      // 
-      this._launchWorkerAtStart.AutoSize = true;
-      this._launchWorkerAtStart.Location = new System.Drawing.Point(3, 256);
-      this._launchWorkerAtStart.Name = "_launchWorkerAtStart";
-      this._launchWorkerAtStart.Size = new System.Drawing.Size(402, 29);
-      this._launchWorkerAtStart.TabIndex = 3;
-      this._launchWorkerAtStart.Text = "Launch Local Rhino.Compute at Start";
-      this._launchWorkerAtStart.UseVisualStyleBackColor = true;
-      // 
-      // _btnClearMemCache
-      // 
-      this._btnClearMemCache.Location = new System.Drawing.Point(3, 154);
-      this._btnClearMemCache.Name = "_btnClearMemCache";
-      this._btnClearMemCache.Size = new System.Drawing.Size(321, 45);
-      this._btnClearMemCache.TabIndex = 4;
-      this._btnClearMemCache.Text = "Clear Hops Memory Cache";
-      this._btnClearMemCache.UseVisualStyleBackColor = true;
-      // 
-      // _lblCacheCount
-      // 
-      this._lblCacheCount.AutoSize = true;
-      this._lblCacheCount.Location = new System.Drawing.Point(333, 164);
-      this._lblCacheCount.Name = "_lblCacheCount";
-      this._lblCacheCount.Size = new System.Drawing.Size(254, 25);
-      this._lblCacheCount.TabIndex = 5;
-      this._lblCacheCount.Text = "(1000000 items in cache)";
-      // 
-      // _childComputeCount
-      // 
-      this._childComputeCount.Location = new System.Drawing.Point(4, 301);
-      this._childComputeCount.Name = "_childComputeCount";
-      this._childComputeCount.Size = new System.Drawing.Size(120, 31);
-      this._childComputeCount.TabIndex = 6;
-      // 
-      // _updateChildCountButton
-      // 
-      this._updateChildCountButton.Location = new System.Drawing.Point(136, 296);
-      this._updateChildCountButton.Name = "_updateChildCountButton";
-      this._updateChildCountButton.Size = new System.Drawing.Size(276, 39);
-      this._updateChildCountButton.TabIndex = 7;
-      this._updateChildCountButton.Text = "Child Process Count";
-      this._updateChildCountButton.UseVisualStyleBackColor = true;
-      // 
-      // _maxConcurrentRequestsTextbox
-      // 
-      this._maxConcurrentRequestsTextbox.Location = new System.Drawing.Point(3, 105);
-      this._maxConcurrentRequestsTextbox.Name = "_maxConcurrentRequestsTextbox";
-      this._maxConcurrentRequestsTextbox.Size = new System.Drawing.Size(80, 31);
-      this._maxConcurrentRequestsTextbox.TabIndex = 8;
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(102, 110);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(262, 25);
-      this.label1.TabIndex = 9;
-      this.label1.Text = "Max Concurrent Requests";
-      // 
-      // HopsAppSettingsUserControl
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this._maxConcurrentRequestsTextbox);
-      this.Controls.Add(this._updateChildCountButton);
-      this.Controls.Add(this._childComputeCount);
-      this.Controls.Add(this._lblCacheCount);
-      this.Controls.Add(this._btnClearMemCache);
-      this.Controls.Add(this._launchWorkerAtStart);
-      this.Controls.Add(this._hideWorkerWindows);
-      this.Controls.Add(this._serversTextBox);
-      this.Name = "HopsAppSettingsUserControl";
-      this.Size = new System.Drawing.Size(599, 348);
-      ((System.ComponentModel.ISupportInitialize)(this._childComputeCount)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this._serversTextBox = new System.Windows.Forms.TextBox();
+            this._hideWorkerWindows = new System.Windows.Forms.CheckBox();
+            this._launchWorkerAtStart = new System.Windows.Forms.CheckBox();
+            this._btnClearMemCache = new System.Windows.Forms.Button();
+            this._lblCacheCount = new System.Windows.Forms.Label();
+            this._childComputeCount = new System.Windows.Forms.NumericUpDown();
+            this._updateChildCountButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this._maxConcurrentRequestsTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._apiKeyTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this._childComputeCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _serversTextBox
+            // 
+            this._serversTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._serversTextBox.Location = new System.Drawing.Point(2, 1);
+            this._serversTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this._serversTextBox.Multiline = true;
+            this._serversTextBox.Name = "_serversTextBox";
+            this._serversTextBox.Size = new System.Drawing.Size(295, 49);
+            this._serversTextBox.TabIndex = 1;
+            // 
+            // _hideWorkerWindows
+            // 
+            this._hideWorkerWindows.AutoSize = true;
+            this._hideWorkerWindows.Location = new System.Drawing.Point(2, 128);
+            this._hideWorkerWindows.Margin = new System.Windows.Forms.Padding(2);
+            this._hideWorkerWindows.Name = "_hideWorkerWindows";
+            this._hideWorkerWindows.Size = new System.Drawing.Size(207, 17);
+            this._hideWorkerWindows.TabIndex = 5;
+            this._hideWorkerWindows.Text = "Hide Rhino.Compute Console Window";
+            this._hideWorkerWindows.UseVisualStyleBackColor = true;
+            // 
+            // _launchWorkerAtStart
+            // 
+            this._launchWorkerAtStart.AutoSize = true;
+            this._launchWorkerAtStart.Location = new System.Drawing.Point(2, 150);
+            this._launchWorkerAtStart.Margin = new System.Windows.Forms.Padding(2);
+            this._launchWorkerAtStart.Name = "_launchWorkerAtStart";
+            this._launchWorkerAtStart.Size = new System.Drawing.Size(204, 17);
+            this._launchWorkerAtStart.TabIndex = 6;
+            this._launchWorkerAtStart.Text = "Launch Local Rhino.Compute at Start";
+            this._launchWorkerAtStart.UseVisualStyleBackColor = true;
+            // 
+            // _btnClearMemCache
+            // 
+            this._btnClearMemCache.Location = new System.Drawing.Point(2, 100);
+            this._btnClearMemCache.Margin = new System.Windows.Forms.Padding(2);
+            this._btnClearMemCache.Name = "_btnClearMemCache";
+            this._btnClearMemCache.Size = new System.Drawing.Size(160, 22);
+            this._btnClearMemCache.TabIndex = 4;
+            this._btnClearMemCache.Text = "Clear Hops Memory Cache";
+            this._btnClearMemCache.UseVisualStyleBackColor = true;
+            // 
+            // _lblCacheCount
+            // 
+            this._lblCacheCount.AutoSize = true;
+            this._lblCacheCount.Location = new System.Drawing.Point(166, 105);
+            this._lblCacheCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this._lblCacheCount.Name = "_lblCacheCount";
+            this._lblCacheCount.Size = new System.Drawing.Size(126, 13);
+            this._lblCacheCount.TabIndex = 5;
+            this._lblCacheCount.Text = "(1000000 items in cache)";
+            // 
+            // _childComputeCount
+            // 
+            this._childComputeCount.Location = new System.Drawing.Point(2, 173);
+            this._childComputeCount.Margin = new System.Windows.Forms.Padding(2);
+            this._childComputeCount.Name = "_childComputeCount";
+            this._childComputeCount.Size = new System.Drawing.Size(60, 20);
+            this._childComputeCount.TabIndex = 7;
+            // 
+            // _updateChildCountButton
+            // 
+            this._updateChildCountButton.Location = new System.Drawing.Point(68, 172);
+            this._updateChildCountButton.Margin = new System.Windows.Forms.Padding(2);
+            this._updateChildCountButton.Name = "_updateChildCountButton";
+            this._updateChildCountButton.Size = new System.Drawing.Size(138, 22);
+            this._updateChildCountButton.TabIndex = 8;
+            this._updateChildCountButton.Text = "Child Process Count";
+            this._updateChildCountButton.UseVisualStyleBackColor = true;
+            // 
+            // _maxConcurrentRequestsTextbox
+            // 
+            this._maxConcurrentRequestsTextbox.Location = new System.Drawing.Point(136, 76);
+            this._maxConcurrentRequestsTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this._maxConcurrentRequestsTextbox.Name = "_maxConcurrentRequestsTextbox";
+            this._maxConcurrentRequestsTextbox.Size = new System.Drawing.Size(42, 20);
+            this._maxConcurrentRequestsTextbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Max Concurrent Requests";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "API Key";
+            // 
+            // _apiKeyTextbox
+            // 
+            this._apiKeyTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._apiKeyTextbox.Location = new System.Drawing.Point(51, 53);
+            this._apiKeyTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this._apiKeyTextbox.Name = "_apiKeyTextbox";
+            this._apiKeyTextbox.Size = new System.Drawing.Size(246, 20);
+            this._apiKeyTextbox.TabIndex = 2;
+            // 
+            // HopsAppSettingsUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._apiKeyTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._maxConcurrentRequestsTextbox);
+            this.Controls.Add(this._updateChildCountButton);
+            this.Controls.Add(this._childComputeCount);
+            this.Controls.Add(this._lblCacheCount);
+            this.Controls.Add(this._btnClearMemCache);
+            this.Controls.Add(this._launchWorkerAtStart);
+            this.Controls.Add(this._hideWorkerWindows);
+            this.Controls.Add(this._serversTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "HopsAppSettingsUserControl";
+            this.Size = new System.Drawing.Size(300, 202);
+            ((System.ComponentModel.ISupportInitialize)(this._childComputeCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -153,5 +189,7 @@ namespace Hops
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.TextBox _maxConcurrentRequestsTextbox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _apiKeyTextbox;
     }
 }
