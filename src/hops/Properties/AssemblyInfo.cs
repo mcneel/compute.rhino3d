eecf3a1eb539d25dcd7ae05c5bcd6f1ded9c57d7
Andrew Payne <andy.payne@mcneel.com>
2022-04-12 14:54:03 -0400

    Updating Hops to 0.13.0

--- src/hops/Properties/AssemblyInfo.cs
+++ src/hops/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ namespace Hops
             TheAssemblyInfo = this;
         }
 
-        public const string AppVersion = "0.12.0.0";
+        public const string AppVersion = "0.13.0.0";
 
         public override Bitmap Icon
         {
