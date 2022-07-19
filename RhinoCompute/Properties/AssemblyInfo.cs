1cc7cf92a17a44693adb0e2abb449c5502dfc21e
BHoMBot <BHoMBot@burohappold.com>
2022-07-19 15:23:59 +0100

    Update project compliance

--- RhinoCompute/Properties/AssemblyInfo.cs
+++ RhinoCompute/Properties/AssemblyInfo.cs
@@ -2,3 +2,5 @@ using System.Reflection;
 
 // NOTE: set by appveyor.yml during ci build
 [assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
+[assembly: AssemblyDescription("https://github.com/BuroHappoldEngineering/RhinoCompute")]
