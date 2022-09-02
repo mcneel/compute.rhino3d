4528dca56ac443e434a75362e3f89eedeb80851c
Alessio Lombardi <alessio.lombardi@burohappold.com>
2022-09-02 17:45:54 +0100

    Fixes: chained IO input management +null checks

--- RhinoCompute_Engine/Compute/RunScript.cs
+++ RhinoCompute_Engine/Compute/RunScript.cs
@@ -23,8 +23,11 @@ namespace BH.Engine.RemoteCompute.RhinoCompute
         private static bool m_askToReenable = true;
 
         [Input("scriptFilePaths", "Scripts to be run. They will be run in the order provided, independently from each other.")]
-        [Input("inputs", "The full list of script will be executed for every input in this list.")]
-        public static List<ComputationOutput> RunScriptChain(List<string> scriptFilePaths, List<CustomObject> inputs = null, bool chainIO = true, GHScriptConfig gHScriptConfig = null, bool active = false)
+        [Input("inputs", "Each script will be executed once for every input in this list.")]
+        [Input("chainIO", "(Optional, false by default) If true, take the outputs of the previously computed script and add them to the inputs to the next script in chain." +
+            "\nNote that the next script in chain may be the same as the previous script run with the next input in the input list, if more than 1 input is specified. " +
+            "\nInputs manually specified take precedence over chained IOs, in case of overlapping names.")]
+        public static List<ComputationOutput> RunScriptChain(List<string> scriptFilePaths, List<CustomObject> inputs = null, bool chainIO = false, GHScriptConfig gHScriptConfig = null, bool active = false)
         {
             if (gHScriptConfig == null)
                 gHScriptConfig = new GHScriptConfig();
@@ -44,7 +47,7 @@ namespace BH.Engine.RemoteCompute.RhinoCompute
 
             // Add empty CustomObject to the inputs to allow computation.
             if (inputs == null || !inputs.Any())
-                inputs.Add(new CustomObject()); 
+                inputs.Add(new CustomObject());
 
             if (inputs.Count <= 1 && scriptFilePaths.Count <= 1)
                 m_askToReenable = false;
@@ -58,12 +61,17 @@ namespace BH.Engine.RemoteCompute.RhinoCompute
                 return emptyOutput;
             }
 
+
             // Each script is to be run once for each input.
+            ComputationOutput scriptResult = new ComputationOutput();
+            string previouslyComputedScript = null;
+
             for (int i = 0; i < scriptFilePaths.Count; i++)
             {
                 string script = scriptFilePaths.ElementAtOrDefault(i);
 
-                ComputationOutput scriptResult = new ComputationOutput();
+                if (string.IsNullOrWhiteSpace(script))
+                    continue;
 
                 for (int j = 0; j < inputs.Count; j++)
                 {
@@ -71,22 +79,27 @@ namespace BH.Engine.RemoteCompute.RhinoCompute
 
                     try
                     {
-                        if (chainIO && i != 0)
+                        if (chainIO && !string.IsNullOrWhiteSpace(previouslyComputedScript) && (scriptResult?.OutputDatas?.Any() ?? false))
                         {
-                            input = new CustomObject();
+                            input = input ?? new CustomObject();
 
-                            foreach (var g in scriptResult.OutputDatas.GroupBy(r => r.Name))
-                                if (input.CustomData.ContainsKey(g.Key))
+                            foreach (var g in scriptResult.OutputDatas?.GroupBy(r => r.Name))
+                                if (!input.CustomData.ContainsKey(g.Key)) // do not overwrite any specified input.
                                     input.CustomData[g.Key] = g.ToList().ToRemoteInputData();
+                                else
+                                    Log.RecordWarning($"The option {nameof(chainIO)} is active, " +
+                                        $"and the script `{Path.GetFileName(previouslyComputedScript)}` returned output `{g.Key}`" +
+                                        $"which conflicts with specified input in `{nameof(inputs)}`.");
                         }
 
                         scriptResult = RunScript(script, input, gHScriptConfig, active);
+                        previouslyComputedScript = script;
                     }
                     catch
                     {
                         m_PartOfChain = j != 0;
                         Log.Clean();
-                        Log.RecordError($"Could not compute script `{Path.GetFileName(scriptFilePaths.ElementAtOrDefault(j))}`.");
+                        Log.RecordError($"Could not compute script `{Path.GetFileName(script)}`.");
                     }
 
                     allOutputs.Add(scriptResult);
