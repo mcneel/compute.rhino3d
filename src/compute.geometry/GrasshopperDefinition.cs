3914d1b1dcd486aaf75c89fc899e44b14b284cbc
Andrew Payne <andy.payne@mcneel.com>
2023-03-02 16:02:23 -0500

    fixing bug so that min and max of a slider are used if a default is not provided

--- src/compute.geometry/GrasshopperDefinition.cs
+++ src/compute.geometry/GrasshopperDefinition.cs
@@ -1461,7 +1461,11 @@ namespace compute.geometry
                     {
                         var val = info.GetValue(par, null);
                         if (val != null)
-                            return Convert.ToDouble(val);
+                        {
+                            var min = Convert.ToDouble(val);
+                            if(min > double.MinValue + Rhino.RhinoMath.Epsilon)
+                                return min;
+                        }
                     }
 
                     if (p.Sources.Count == 1)
@@ -1486,7 +1490,11 @@ namespace compute.geometry
                     {
                         var val = info.GetValue(par, null);
                         if (val != null)
-                            return Convert.ToDouble(val);
+                        {
+                            var max = Convert.ToDouble(val);
+                            if (max < double.MaxValue - Rhino.RhinoMath.Epsilon)
+                                return max;
+                        }
                     }
 
                     if (p.Sources.Count == 1)
