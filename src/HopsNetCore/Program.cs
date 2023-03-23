3e473341617d186140f18380141a09fa03d6fff7
Curtis Wensley <curtis.wensley@gmail.com>
2023-03-23 11:04:10 -0700

    Add a .NET Core launcher project

---
+++ src/HopsNetCore/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// NOTE: This project is only used to launch Rhino using the .NET Core debugger and is never run directly
