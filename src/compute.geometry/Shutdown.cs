5c5c22e1b75f8bce2cd27e2adb891577c58694dc
Andrew Payne <andy.payne@mcneel.com>
2025-02-21 09:52:58 -0500

    Adding logging information for idlespan and shutdown process

--- src/compute.geometry/Shutdown.cs
+++ src/compute.geometry/Shutdown.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using Microsoft.Extensions.Hosting;
 
 namespace compute.geometry
@@ -60,7 +61,7 @@ namespace compute.geometry
         static System.Net.Http.HttpClient _httpClient;
         static DateTime _lastSpanCheck = DateTime.Now;
 
-        private static void TimerTask(object timerState)
+        private async static void TimerTask(object timerState)
         {
             bool shutdown = false;
 
@@ -79,38 +80,52 @@ namespace compute.geometry
                 // Don't check the server every timer tick. Just check when we start approaching
                 // what we think is our span limit.
                 var shouldCheckSpan = DateTime.Now - _lastSpanCheck;
-                //Serilog.Log.Information($"Idle span value is {_idleSpan}. Elapsed time {shouldCheckSpan.TotalSeconds} seconds. Last span check {_lastSpanCheck.ToLocalTime()}.");
+                //Serilog.Log.Debug($"Idle span value is {_idleSpan}. Elapsed time {shouldCheckSpan.TotalSeconds} seconds. Last span check {_lastSpanCheck.ToLocalTime()}.");
                 if (shouldCheckSpan.TotalSeconds > _idleSpan)
                 {
                     if (_httpClient == null)
                         _httpClient = new System.Net.Http.HttpClient();
                     string url = $"http://localhost:{_parentPort}/idlespan";
                     _lastSpanCheck = DateTime.Now;
+                    Serilog.Log.Debug($"The elapsed time has exceeded the idle span limit");
+                    Serilog.Log.Debug($"Checking with parent process at {url}.");
                     try
                     {
                         if (!String.IsNullOrEmpty(Config.ApiKey) && !_httpClient.DefaultRequestHeaders.Contains("RhinoComputeKey"))
                             _httpClient.DefaultRequestHeaders.Add("RhinoComputeKey", Config.ApiKey);
-                        string span = _httpClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
-                        int serverIdleSpan = int.Parse(span);
-                        if (serverIdleSpan + (serverIdleSpan * 0.1) > _idleSpan)
+                        HttpResponseMessage response = await _httpClient.GetAsync(url);
+                        response.EnsureSuccessStatusCode();    
+                        string span = response.Content.ReadAsStringAsync().Result;
+                        if (!string.IsNullOrEmpty(span))
                         {
-                            shutdown = true;
+                            int serverIdleSpan = int.Parse(span);
+                            Serilog.Log.Debug($"Response received. Parent process idle span is {serverIdleSpan} seconds.");
+                            if (serverIdleSpan + (serverIdleSpan * 0.1) > _idleSpan)
+                            {
+                                Serilog.Log.Debug("Idle span limit reached");
+                                shutdown = true;
+                            }
+                        }
+                        else
+                        {
+                            Serilog.Log.Debug("Idle span value from parent process was null or empty");
                         }
                     }
                     catch(Exception)
                     {
-                        // not sure what to do here
+                        Serilog.Log.Debug($"Failed to get response from {url}");
                     }
                 }
             }
 
             if (shutdown)
             {
+                Serilog.Log.Debug("Shutting down child process");
                 var elapsedTime = DateTime.Now - _startTime;
                 Serilog.Log.Information("Total elapsed time for child process is " + string.Format("{0:D2} days, {1:D2} hrs, {2:D2} mins, {3:D2} secs", elapsedTime.Days, elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds));
                 var app = timerState as IHost;
                 if (app != null)
-                    app.StopAsync();
+                    await app.StopAsync();
             }
         }
     }
