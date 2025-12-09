ed9d6a42823f78e8f4f76e853a25d9ab0e5400f6
Luis E. Fraguada <luis@mcneel.com>
2025-12-09 13:50:21 +0100

    Ensure all of this still works on WIndows

--- src/compute.geometry/IO/GhPath.cs
+++ src/compute.geometry/IO/GhPath.cs
@@ -117,10 +117,14 @@ namespace Resthopper.IO
                             }
                             else if(goo is IGH_ReferencedData refData && refData.IsReferencedData)
                             {
-                                //var modelData = refData as Grasshopper.Rhinoceros.ModelContent;
-                                //list[i] = modelData.AsFrozen(true);
+// Changing this depends on supporting a more recent version of the GH nuget package on Linux
+#if !LINUX
+                                var modelData = refData as Grasshopper.Rhinoceros.ModelContent;
+                                list[i] = modelData.AsFrozen(true);
+#else
                                 var modelData = refData as Grasshopper.Rhinoceros.ModelData;
                                 list[i] = modelData.ToAttributes().ToModelData();
+#endif
                             }
                         }
                     }
